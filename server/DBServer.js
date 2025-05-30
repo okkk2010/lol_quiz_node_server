df500603fce28751fa10391d4a67da56bb93ca4e
okkk2010 <okkk2010@naver.com>
2025-05-31 00:59:01 +0900

    2025.05.31
    postman test success
    heroku hosting test

--- server/DBServer.js
+++ server/DBServer.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
