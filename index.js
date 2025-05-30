ab519c21faebae743621cd662e64a57288f7ad80
okkk2010 <okkk2010@naver.com>
2025-05-31 01:21:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 // app.set('view engine', 'ejs');
 // app.use(express.static('public'));
 // app.use(bodyParser.urlencoded({ extended: false }));
